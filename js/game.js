07b04d888745038afe25186d3f4fff00f20e514e
Benjamin Landers <benjaminrlanders@gmail.com>
2015-08-22 11:36:26 -0700

    no flying shia

--- js/game.js
+++ js/game.js
@@ -43,9 +43,11 @@ function create() {
   player.body.setSize(20, 32, 5, 27);
   player.animations.add('left', [0, 1, 2, 3], 5, true);
   player.animations.add('left-sprint', [9, 10, 11,11], 8, true);
+  player.animations.add('left-pounce', [9, 10, 11], 8, false);
   player.animations.add('turn', [4], 20, true);
   player.animations.add('right', [5, 6, 7, 8], 5, true);
   player.animations.add('right-sprint', [12, 13, 14,14], 8, true);
+  player.animations.add('right-pounce', [12, 13, 14], 8, false);
 
   // Generate the ground
   ground = game.add.group();
@@ -82,8 +84,10 @@ function create() {
 
   // Player functions
   player.pounce = function() {
-    this.body.velocity.y = -250;
-  }
+    this.body.velocity.y = -300;
+    this.body.velocity.x = 450*((facing === 'left')?-1:1);
+    player.animations.play(facing+"-pounce");
+  };
   player.moveLeft = function() {
     if (wasd.shift.isDown) {
         this.body.velocity.x = -sprintSpeed;
@@ -93,7 +97,7 @@ function create() {
       player.animations.play('left');
     }
     facing = 'left';
-  }
+  };
   player.moveRight = function() {
     if (wasd.shift.isDown) {
         this.body.velocity.x = sprintSpeed;
@@ -103,44 +107,39 @@ function create() {
       player.animations.play('right');
     }
     facing = 'right';
-  }
+  };
 }
 
 function update() {
   game.camera.focusOnXY(player.position.x, game.world.height / 2);
 
   game.physics.arcade.collide(player, ground);
-
-  player.body.velocity.x = 0;
-
-  if (cursors.left.isDown || wasd.left.isDown) {
-    player.moveLeft();
-  }
-  else if (cursors.right.isDown || wasd.right.isDown) {
-    player.moveRight();
-  }
-  else {
-    if (facing != 'idle') {
-      player.animations.stop();
-
-      if (facing == 'left') {
-        player.frame = 0;
-      }
-      else {
-        player.frame = 5;
-      }
-      facing = 'idle';
+  
+  if(player.body.touching.down){
+    if (cursors.left.isDown || wasd.left.isDown) {
+      player.moveLeft();
     }
-  }
-
-  // Player's leaping velocity
-  if(!player.body.touching.down && wasd.shift.isDown) {
-    player.body.velocity.x = player.body.velocity.x * pounceSpeedMultiplier;
-  }
-
-  if ((wasd.up.isDown || cursors.up.isDown || jumpButton.isDown) && player.body.touching.down) {
-    console.log('jumping');
-    player.pounce();
+    else if (cursors.right.isDown || wasd.right.isDown) {
+      player.moveRight();
+    }
+    else {
+      if (facing != 'idle') {
+        player.animations.stop();
+        player.body.velocity.x = 0;
+        if (facing == 'left') {
+          player.frame = 0;
+        }
+        else {
+          player.frame = 5;
+        }
+        facing = 'idle';
+      }
+    }
+  
+    if ((wasd.up.isDown || cursors.up.isDown || jumpButton.isDown)&&wasd.shift.isDown) {
+      console.log('jumping');
+      player.pounce();
+    }
   }
 
 }
